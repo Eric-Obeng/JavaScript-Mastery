e6929a2f29fb913453ee34ce9e999cc6413627d6
Eric-Obeng <obengeric.3958@gmail.com>
2024-02-07 23:34:28 +0000

    beginning of chapter 2

---
+++ Chapter-2/index.js
@@ -0,0 +1,26 @@
+// Program Structure
+// Statement and Expression
+
+// Bindings Or variable
+let caught = 5 * 5;
+console.log(caught);
+
+//variable Keywords
+/****
+ * let
+ * const
+ * var
+ */
+
+let ten = 10;
+console.log(ten + 2); //12
+console.log(ten * ten); //100
+console.log(ten);
+
+{
+  let mood = "light";
+  console.log(mood);
+  mood = "dark";
+  mood = "darker";
+  console.log(mood);
+}
