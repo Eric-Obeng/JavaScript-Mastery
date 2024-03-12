7d08f37948aa2ef219e52c36ce563b06cf2b5006
Eric-Obeng <obengeric.3958@gmail.com>
2024-03-12 23:35:38 +0000

    for loops

--- Chapter-2/index.js
+++ Chapter-2/index.js
@@ -79,37 +79,37 @@ console.log(Math.min(2, 4));
 
 //Control Flow
 
-{
-  //let, const, var
-  // let theNumber = Number(prompt("Pick a number"));
+//let, const, var
+// let theNumber = Number(prompt("Pick a number"));
 
-  // console.log(`Your number is the squared root of ${theNumber ** 2}`);
+// console.log(`Your number is the squared root of ${theNumber ** 2}`);
 
-  //Conditional Executions
-  // let number = Number(prompt("Pick a number"));
+//Conditional Executions
+// let number = Number(prompt("Pick a number"));
 
-  // if (!Number.isNaN(number)) {
-  //   console.log("Your number is the square root of " + number * number);
-  // } else {
-  //   console.log("Enter a number");
-  // }
+// if (!Number.isNaN(number)) {
+//   console.log("Your number is the square root of " + number * number);
+// } else {
+//   console.log("Enter a number");
+// }
 
-  // 80 - 100 = 'A', 70 - 79 = 'B', 50- 69 = 'C' , anything else = 'Fail'
-  // let mark = Number(prompt("Enter your mark...."));
+// 80 - 100 = 'A', 70 - 79 = 'B', 50- 69 = 'C' , anything else = 'Fail'
+// let mark = Number(prompt("Enter your mark...."));
 
-  // if (Number.isNaN(mark)) {
-  //   console.log("Enter a number");
-  // } else if (mark >= 80) {
-  //   console.log("A");
-  // } else if (mark >= 70) {
-  //   console.log("B");
-  // } else if (mark >= 50) {
-  //   console.log("C");
-  // } else {
-  //   console.log("Fail");
-  // }
+// if (Number.isNaN(mark)) {
+//   console.log("Enter a number");
+// } else if (mark >= 80) {
+//   console.log("A");
+// } else if (mark >= 70) {
+//   console.log("B");
+// } else if (mark >= 50) {
+//   console.log("C");
+// } else {
+//   console.log("Fail");
+// }
 
-  //While and Do loop
+//While and Do loop
+/*
   {
     let number = 0; //2,4,6,8,10,12,14
 
@@ -131,4 +131,56 @@ console.log(Math.min(2, 4));
 
     
   }
+
+  
+  
+*/
+console.log("\n");
+
+//For loop
+{
+  //initialization
+  //condition
+  //increment or decrement
+
+  for (let number = 0; number <= 12; number += 2) {
+    console.log(number);
+  }
+  console.log("\n");
+
+  let result = 1;
+  for (let counter = 0; counter < 10; counter++) {
+    result *= 2;
+  }
+  console.log(result);
+}
+
+//Breaking out of a loop
+{
+  for (let current = 20; ; current++) {
+    if (current % 7 == 0) {
+      console.log(current);
+      break;
+    }
+  }
+}
+
+console.log("\n");
+
+//Updating bindings
+{
+  let number = 5; //5, 4
+  let newNumber = --number; //3
+  let newNumber2 = newNumber--; //4
+
+  console.log(newNumber); //3
+  console.log(newNumber2); //4
+  console.log(number); //4, 5
+
+  //4, -5,
+  // --variable, variable--
+  //pre decrement
+  //post decrement
+
+  
 }
